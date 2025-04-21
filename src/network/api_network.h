b56e37ca5dd297e334645c3978648a236ee46197
fangyefeng <1250890838@qq.com>
2025-04-21 10:05:53 +0800

    add more api

--- src/network/api_network.h
+++ src/network/api_network.h
@@ -2,25 +2,27 @@
 #define NETWORK_API_H
 
 #include <qstringliteral.h>
-#include <QStringLiteral>
 #include <QString>
+#include <QStringLiteral>
 
 namespace network_api {
-    const QString host=QStringLiteral("http://localhost:3000");
+const QString host = QStringLiteral("http://localhost:3000");
 
-    const QString apiGetQRCode=host + QStringLiteral("/login/qr/key");
-    const QString apiCreateQRCode=host + QStringLiteral("/login/qr/create");
-    const QString apiCheckQRCode=host + QStringLiteral("/login/qr/check");
-    const QString apiGetHighqualityPlaylists=host+QStringLiteral("/top/playlist/highquality");
-    const QString apiGetSelectivePlaylists=host+QStringLiteral("/top/playlist");
-    const QString apiCatlist=host+QStringLiteral("/playlist/catlist");
-    const QString apiPlaylistDetail=host+QStringLiteral("/playlist/detail");
-    const QString apiPlaylistTracks=host+QStringLiteral("/playlist/track/all");
+const QString apiGetQRCode = host + QStringLiteral("/login/qr/key");
+const QString apiCreateQRCode = host + QStringLiteral("/login/qr/create");
+const QString apiCheckQRCode = host + QStringLiteral("/login/qr/check");
+const QString apiGetHighqualityPlaylists =
+    host + QStringLiteral("/top/playlist/highquality");
+const QString apiGetSelectivePlaylists = host + QStringLiteral("/top/playlist");
+const QString apiCatlist = host + QStringLiteral("/playlist/catlist");
+const QString apiPlaylistDetail = host + QStringLiteral("/playlist/detail");
+const QString apiPlaylistTracks = host + QStringLiteral("/playlist/track/all");
+const QString apiPlaylistComments = host + QStringLiteral("/comment/playlist");
 
-    const QString getSongUrl=host+QStringLiteral("/song/url/v1");
-    const QString getSongComment=host+QStringLiteral("/lyric");
-    const QString getPlaylistComment=host+QStringLiteral("comment/playlist");
-    const QString getAlbumComment=host+QStringLiteral("/comment/album");
-    }
+const QString getSongUrl = host + QStringLiteral("/song/url/v1");
+const QString getSongComment = host + QStringLiteral("/lyric");
+const QString getPlaylistComment = host + QStringLiteral("comment/playlist");
+const QString getAlbumComment = host + QStringLiteral("/comment/album");
+}  // namespace network_api
 
 #endif
