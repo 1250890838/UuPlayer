e439cf5fa597ddb58028a642a0f41b3343fb1772
fangyefeng <1250890838@qq.com>
2025-09-16 23:03:05 +0800

    Add methods to class MediaItemFilterProxyModel

--- src/service/model/mediaitem_filterproxy_model.cc
+++ src/service/model/mediaitem_filterproxy_model.cc
@@ -1,5 +1,42 @@
 #include "mediaitem_filterproxy_model.h"
 
+void MediaItemFilterProxyModel::sortByArtistName(Qt::SortOrder order)
+{
+  QHash<int,QByteArray> roleNames = sourceModel()->roleNames();
+  int role = roleNames.key("artists");
+  this->setSortRole(role);
+  this->sort(0,order);
+}
+
+void MediaItemFilterProxyModel::sortByTitle(Qt::SortOrder order)
+{
+  QHash<int,QByteArray> roleNames = sourceModel()->roleNames();
+  int role = roleNames.key("name");
+  this->setSortRole(role);
+  this->sort(0,order);
+}
+
+void MediaItemFilterProxyModel::sortByDuration(Qt::SortOrder order)
+{
+  QHash<int,QByteArray> roleNames = sourceModel()->roleNames();
+  int role = roleNames.key("duration");
+  this->setSortRole(role);
+  this->sort(0,order);
+}
+
+void MediaItemFilterProxyModel::sortByFileSize(Qt::SortOrder order)
+{
+  //TODO:
+}
+
+void MediaItemFilterProxyModel::sortByAlbumName(Qt::SortOrder order)
+{
+  QHash<int,QByteArray> roleNames = sourceModel()->roleNames();
+  int role = roleNames.key("album");
+  this->setSortRole(role);
+  this->sort(0,order);
+}
+
 MediaItemFilterProxyModel::MediaItemFilterProxyModel()
 {
 
--- src/service/model/mediaitem_filterproxy_model.h
+++ src/service/model/mediaitem_filterproxy_model.h
@@ -7,6 +7,12 @@ class MediaItemFilterProxyModel : public QSortFilterProxyModel
 {
   Q_OBJECT
  public:
+  Q_INVOKABLE void sortByArtistName(Qt::SortOrder);
+  Q_INVOKABLE void sortByTitle(Qt::SortOrder);
+  Q_INVOKABLE void sortByDuration(Qt::SortOrder);
+  Q_INVOKABLE void sortByFileSize(Qt::SortOrder);
+  Q_INVOKABLE void sortByAlbumName(Qt::SortOrder);
+ public:
   MediaItemFilterProxyModel();
 };
 
