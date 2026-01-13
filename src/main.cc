6ec886e7230042225df779dcba4bb6c428ca769c
fangyefeng <1250890838@qq.com>
2026-01-13 21:16:38 +0800

    format; remove redunant comment

--- src/main.cc
+++ src/main.cc
@@ -9,8 +9,8 @@
 
 #include "network/basic_network.h"
 #include "service/login_service.h"
-#include "service/song_service.h"
 #include "service/model/mediaitem_filterproxy_model.h"
+#include "service/song_service.h"
 
 #ifdef Q_OS_WIN
 #include <Windows.h>
@@ -23,34 +23,21 @@ int main(int argc, char* argv[]) {
   QQmlApplicationEngine engine;
   QWK::registerTypes(&engine);
   engine.addImportPath(u"qrc:/gui/qml"_qs);  // register custom components
-
   qmlRegisterUncreatableMetaObject(network::error_code::staticMetaObject,
                                    "network.errorcode", 1, 0, "ErrorCode",
                                    "Error: only error code enums");
-  qmlRegisterUncreatableMetaObject(sort_option::staticMetaObject,"model.sortoption",1,0,"SortOption","Error: only sort option enums");
-      /*
-  qmlRegisterUncreatableMetaObject(service::PlayService::staticMetaObject,  // 元对象
-                                   "service.api", 1, 0, "PlayService",
-                                   "Cannot create PlayServiceEnums");
-  */
+  qmlRegisterUncreatableMetaObject(sort_option::staticMetaObject,
+                                   "model.sortoption", 1, 0, "SortOption",
+                                   "Error: only sort option enums");
 
-      qmlRegisterSingletonType(QUrl(u"qrc:/gui/qml/assets/Icons.qml"_qs), "assets",
-                               1, 0, "Icons");
+  qmlRegisterSingletonType(QUrl(u"qrc:/gui/qml/assets/Icons.qml"_qs), "assets",
+                           1, 0, "Icons");
   qmlRegisterSingletonType(QUrl(u"qrc:/gui/qml/assets/Skins.qml"_qs), "skins",
                            1, 0, "Skins");
-  /*
-  qRegisterMetaType<model::MediaItem*>("MediaItem*");
-  qmlRegisterSingletonInstance<service::LoginService>("service", 1, 0, "LoginService",new service::LoginService());
-  qmlRegisterSingletonInstance<service::PlaylistService>("service", 1, 0, "PlaylistsService", new service::PlaylistService());
-  qmlRegisterSingletonInstance<service::SongService>(
-    "service", 1, 0, "SongService", new service::SongService());
-  */
-
   const QUrl url(u"qrc:/gui/qml/Main.qml"_qs);
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
       []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
   engine.load(url);
-
   return app.exec();
 }
