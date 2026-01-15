4cf64a7420ae4e4d488acbd8c2c47323a9104b9d
fangyefeng <1250890838@qq.com>
2026-01-15 20:37:49 +0800

    fix thread affinity warning by marshaling network requests to main thread

--- src/gui/image_provider.cc
+++ src/gui/image_provider.cc
@@ -1,5 +1,5 @@
 #include "image_provider.h"
-
+#include <QCoreApplication>  // 用于获取主线程上下文（如果需要），这里直接用 network 对象即可
 #include <QNetworkReply>
 #include <QtConcurrent/QtConcurrent>
 
@@ -8,35 +8,38 @@ namespace gui {
 NetworkImageResponse::NetworkImageResponse(const QString& id,
                                            network::BasicNetwork* network,
                                            QSize requestedSize)
-    : m_network(network), m_requestedSize(requestedSize) {
+    : m_requestedSize(requestedSize) {
   QUrl url(id);
   QNetworkRequest req(url);
-  m_reply = m_network->get(req);
-  m_network->connect(m_reply, &QNetworkReply::finished, this,
-                     &NetworkImageResponse::handleNetFinished);
-  connect(m_reply, &QNetworkReply::errorOccurred, this,
-          [this](QNetworkReply::NetworkError) { emit finished(); });
+  QMetaObject::invokeMethod(
+      network,
+      [this, req, network]() {
+        QNetworkReply* reply = network->get(req);
+        connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+          if (reply->error() == QNetworkReply::NoError) {
+            QByteArray data = reply->readAll();
+            QtConcurrent::run([this, data]() {
+              QImage tempImage;
+              tempImage.loadFromData(data);
+              if (m_requestedSize.isValid() && !tempImage.isNull()) {
+                tempImage =
+                    tempImage.scaled(m_requestedSize, Qt::KeepAspectRatio,
+                                     Qt::SmoothTransformation);
+              }
+              m_image = tempImage;
+              emit finished();
+            });
+          } else {
+            qDebug() << "Network Error:" << reply->errorString();
+            emit finished();
+          }
+          reply->deleteLater();
+        });
+      },
+      Qt::QueuedConnection);
 }
 
-void NetworkImageResponse::handleNetFinished() {
-  if (m_reply->error() == QNetworkReply::NoError) {
-    QByteArray data = m_reply->readAll();
-    QtConcurrent::run([this, data]() {
-      QImage tempImage;
-      tempImage.loadFromData(data);
-      if (m_requestedSize.isValid() && !tempImage.isNull()) {
-        tempImage = tempImage.scaled(m_requestedSize, Qt::KeepAspectRatio,
-                                     Qt::SmoothTransformation);
-      }
-      m_image = tempImage;
-      emit finished();
-    });
-  } else {
-    qDebug() << "Network Error:" << m_reply->errorString();
-  }
-  m_reply->deleteLater();
-  m_reply = nullptr;
-}
+void NetworkImageResponse::handleNetFinished() {}
 
 QQuickTextureFactory* NetworkImageResponse::textureFactory() const {
   return QQuickTextureFactory::textureFactoryForImage(m_image);
@@ -52,4 +55,5 @@ QQuickImageResponse* ImageProvider::requestImageResponse(
     const QString& id, const QSize& requestedSize) {
   return new NetworkImageResponse(id, m_network, requestedSize);
 }
+
 }  // namespace gui
