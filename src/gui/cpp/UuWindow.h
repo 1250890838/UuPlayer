10f3b35f4fb2be71136e59de6f9bd949998d2253
fangyefeng <1250890838@qq.com>
2026-01-18 21:11:46 +0800

    format

--- src/gui/cpp/UuWindow.h
+++ src/gui/cpp/UuWindow.h
@@ -1,19 +1,20 @@
 #include <QtQml/qqmlregistration.h>
+#include <QMouseEvent>
 #include <QObject>
 #include <QQuickWindow>
-#include <QMouseEvent>
 
-class UuWindow : public QQuickWindow
-{
-    Q_OBJECT
-    QML_ELEMENT
-public:
-    UuWindow(QWindow *parent = nullptr);
-Q_INVOKABLE void applyRadius(qint32 radius);
-protected:
-    virtual void mouseMoveEvent(QMouseEvent *event) override;
-    virtual void mousePressEvent(QMouseEvent *event) override;
-    virtual void mouseReleaseEvent(QMouseEvent *event) override;
-private:
-    QPointF m_lastPos;
+class UuWindow : public QQuickWindow {
+  Q_OBJECT
+  QML_ELEMENT
+ public:
+  UuWindow(QWindow* parent = nullptr);
+  Q_INVOKABLE void applyRadius(qint32 radius);
+
+ protected:
+  virtual void mouseMoveEvent(QMouseEvent* event) override;
+  virtual void mousePressEvent(QMouseEvent* event) override;
+  virtual void mouseReleaseEvent(QMouseEvent* event) override;
+
+ private:
+  QPointF m_lastPos;
 };
