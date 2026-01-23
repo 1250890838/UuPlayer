fa7a5023d68b17245033bc46e815ec46852117e2
fangyefeng <1250890838@qq.com>
2026-01-23 21:52:35 +0800

    tweak

--- src/controller/featured_playlisthub_controller.cc
+++ src/controller/featured_playlisthub_controller.cc
@@ -2,4 +2,8 @@
 namespace controller {
 
 FeaturedPlaylistHubController::FeaturedPlaylistHubController() {}
+
+PlaylistItemModel* FeaturedPlaylistHubController::currPlaylistItems() {
+  return &m_playlistItemModel;
+}
 }  // namespace controller
--- src/controller/featured_playlisthub_controller.h
+++ src/controller/featured_playlisthub_controller.h
@@ -3,14 +3,37 @@
 
 #include <QObject>
 #include <QQmlEngine>
+
+#include "model/playlist_item_model.h"
+
+#include "recommended_playlist_service.h"
+
 namespace controller {
 
+using namespace model;
+using namespace service;
 class FeaturedPlaylistHubController : public QObject {
   Q_OBJECT
   QML_ELEMENT
   QML_SINGLETON
+  Q_PROPERTY(PlaylistItemModel* currPlaylistItems READ currPlaylistItems)
+  Q_PROPERTY(QVariantMap categories READ categories NOTIFY categoriesChanged)
+ public:
+  Q_INVOKABLE void fetchPlaylistItems(const QString& tag);
+  Q_INVOKABLE void fetchCategories();
+
  public:
   FeaturedPlaylistHubController();
+  PlaylistItemModel* currPlaylistItems();
+  QVariantMap categories();
+
+ signals:
+  void categoriesChanged();
+
+ private:
+  RecommendedPlaylistService m_recommendedPlaylistService;
+
+  PlaylistItemModel m_playlistItemModel;
 };
 }  // namespace controller
 #endif // PLAYLISTHUB_H
